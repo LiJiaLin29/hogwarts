02467322d3b08e1c9717359709132359e1c8219a
lijialin <lijlperson@163.com>
2020-08-11 12:12:21 +0800

    回合游戏2.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hogwarts
+# 第一次作业
+./fight_game
 python学习代码
--- fight_game.py
+++ fight_game.py
@@ -3,27 +3,31 @@
  规则：1.hp的初始值为1000，power的初始值为200
       2.打斗多个回合，直到一方血量为0
 """
-my_hp, enemy_hp = 1000 #血量
-my_power, enemy_power = 200 #攻击力
 
-# 打斗函数
-def fight():
+my_hp, enemy_hp = 1000, 1000  # 血量
+my_power, enemy_power = 200, 200  # 攻击力
 
+
+def fight():  # 打斗函数
+
+    global my_hp, enemy_hp, my_power, enemy_power
     while True:
         # 双方互相攻击
         my_hp = my_hp - enemy_power
         enemy_hp = enemy_hp - my_power
+        print(f"受到{enemy_power}点攻击，我的剩余血量{[0 if my_hp < 0 else my_hp]}")
+        print(f"受到{my_power}点攻击，敌方的剩余血量{[0 if enemy_hp < 0 else enemy_hp]}")
         # 判断游戏是否结束
         if my_hp > 0 and enemy_hp <= 0:
-            print("我赢了");break;
-        elif my_hp = 0 and enemy_hp = 0:
-            print("打成平手");break;
-        elif my_hp <= 0 and enemy_hp > 0
-            print("敌方赢了");break;
+            print("我赢了")
+            break
+        elif my_hp == 0 and enemy_hp == 0:
+            print("打成平手")
+            break
+        elif my_hp <= 0 and enemy_hp > 0:
+            print("敌方赢了")
+            break
+
 
 # 调用函数
 fight()
-
-
-
-
