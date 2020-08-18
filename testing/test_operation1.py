94df62591321a2153880497b29a58aaea1b42b0a
lijialin <lijlperson@163.com>
2020-08-18 20:37:23 +0800

    pytest实战一

--- testing/test_operation1.py
+++ testing/test_operation1.py
@@ -1,12 +1,12 @@
 """
- @description: 测试计算器加法、减法、乘法、除法功能
+ @description: 测试计算器加法、除法功能
+ # 使用setup(), teardown() , setup_class(), teardown_class()
  @auther: 29263
- @date: 2020-08-18 20:29
+ @date: 2020-08-17 15:28
 """
 import pytest
-
 from mathUtil.operations import *
-from testing1.conftest import *
+from testing.conftest import *
 
 
 class TestA:
@@ -38,42 +38,12 @@ class TestA:
         err_msg = e.value.args[0]
         assert err_msg == "输入包含非数字，类型错误"
 
-    @pytest.mark.parametrize('x, y, expected', yaml_data_with_key('valid_sub'),
-                             ids=['整数', '小数', '小数整数', '字符串整数', '字符串小数'])
-    def test_sub(self, x, y, expected):
-        r = sub(x, y)
-        print(f"{x}-{y}=", r)
-        assert r == expected
-
-    @pytest.mark.parametrize('x, y', [('2', 'a')], ids=['包含非数字异常'])
-    def test_sub_except1(self, x, y):
-        # 预期的异常
-        with pytest.raises(TypeError) as e:
-            sub(x, y)
-        err_msg = e.value.args[0]
-        assert err_msg == "输入包含非数字，类型错误"
-
-    @pytest.mark.parametrize('x, y, expected', yaml_data_with_key('valid_mul'),
-                             ids=['整数', '小数', '小数整数', '字符串整数', '字符串小数'])
-    def test_mul(self, x, y, expected):
-        r = mul(x, y)
-        print(f"{x}*{y}=", r)
-        assert r == expected
-
-    @pytest.mark.parametrize('x, y', [('2', 'a')], ids=['包含非数字异常'])
-    def test_mul_except1(self, x, y):
-        # 预期的异常
-        with pytest.raises(TypeError) as e:
-            mul(x, y)
-        err_msg = e.value.args[0]
-        assert err_msg == "输入包含非数字，类型错误"
-
     @pytest.mark.parametrize('x, y, expected', yaml_data_with_key('valid_div'),
                              ids=['整数', '小数', '小数整数', '字符串整数', '字符串小数'])
     def test_div(self, x, y, expected):
-        r = div(x, y)
-        print(f"{x}/{y}=", r)
-        assert r == expected
+            r = div(x, y)
+            print(f"{x}/{y}=", r)
+            assert r == expected
 
     @pytest.mark.parametrize('x, y', [('2', 0)], ids=['除数为0异常'])
     def test_div_except1(self, x, y):
@@ -90,3 +60,6 @@ class TestA:
             div(x, y)
         err_msg = e.value.args[0]
         assert err_msg == "输入包含非数字，类型错误"
+
+
+
